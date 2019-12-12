99c7471c65d6588211aef3bbae8a2f827a5acf30
Kamran Mmmdv <amorfrap@gmail.com>
2019-12-12 19:38:14 +0400

    Corrected typo

--- ccproject/settings.py
+++ ccproject/settings.py
@@ -85,7 +85,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'ccproect',
+            'NAME': 'ccproject',
             'USER': 'kerimov',
             'PASSWORD': 'admin123',
             'HOST': 'localhost',
