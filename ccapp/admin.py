ee1ae641102eec92d4f078f44b76c9d8b351a7e5
Kamran Mmmdv <amorfrap@gmail.com>
2019-12-12 21:19:02 +0400

    Deleted applicant status from admin dashboard

--- ccapp/admin.py
+++ ccapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question, Applicant, ApplicantStatus, Message
+from .models import Question, Applicant, Message
 from django.contrib.auth.models import Group, User
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -11,13 +11,9 @@ class ApplicantAdmin(admin.ModelAdmin):
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('sender_name', 'message_text', 'sender_email')
 
-class ApplicantStatusAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-
 admin.site.site_header = "Challengers Club - Admin Dashboard" 
 admin.site.unregister(Group)
 admin.site.unregister(User)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Applicant, ApplicantAdmin)
 admin.site.register(Message, MessageAdmin)
-admin.site.register(ApplicantStatus, ApplicantStatusAdmin)
