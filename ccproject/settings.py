e8e4caa5337acc6960f6affa64c4712ba3fbac29
Kamran Mmmdv <amorfrap@gmail.com>
2019-12-14 22:58:46 +0400

    Changed emailing configurations

--- ccproject/settings.py
+++ ccproject/settings.py
@@ -93,22 +93,13 @@ else:
         }
     }
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# SERVER_EMAIL = 'hi@challengers.az'
-# EMAIL_HOST_USER = 'hi@challengers.az'
-# EMAIL_HOST_PASSWORD = 'Qartdere1'
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = '	smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-SERVER_EMAIL = 'apikey'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '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'
-EMAIL_USE_TLS = True
+SERVER_EMAIL = 'kamransrecovery@gmail.com'
+EMAIL_HOST_USER = 'kamransrecovery@gmail.com'
+EMAIL_HOST_PASSWORD = 'kamran284'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
