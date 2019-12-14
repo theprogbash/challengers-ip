bff9ac978b390b3538d6625964fa167a3a2d2c26
Kamran Mmmdv <amorfrap@gmail.com>
2019-12-14 23:24:51 +0400

    Changed email configs

--- ccproject/settings.py
+++ ccproject/settings.py
@@ -95,7 +95,7 @@ else:
 
 ADMINS = [('Samir', 'kamransrecovery@gmail.com')]
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
