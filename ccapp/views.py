f4f68e47c0bfed1b56c63e2b3168278d4dae1d7b
Kamran Mmmdv <amorfrap@gmail.com>
2019-12-15 11:20:22 +0400

    Added template for applicatns

--- ccapp/views.py
+++ ccapp/views.py
@@ -11,20 +11,28 @@ def index(request):
         'empty_cards': range(4 - Applicant.objects.filter(status=1).count())
     }
     if request.method == "POST":
+        applicant_email = request.POST.get('email')
+        applicant_name = request.POST.get('name')
+        applicant_surname = request.POST.get('surname')
+        subject = 'Challengers Club-a xoş gəldin !'
+        message = 'Hörmətli ' + str(applicant_name) + ' ' + str(applicant_surname) + '! \nBu marafonda iştirak etməyə baş vurduğun üçün təşəkkür edirik. Tezliklə sənə statusun barədə məlumat veriləcək.'
+        from_email = settings.SERVER_EMAIL
+        recipient_list = [applicant_email]
+        send_mail(subject, message, from_email, recipient_list)
         if request.POST.get('motivation_letter'):
             Applicant.objects.create(
-            name = request.POST.get('name'),
-            surname = request.POST.get('surname'),
-            birth_date = request.POST.get('birth_date'),
-            phone = request.POST.get('phone'),
-            email = request.POST.get('email'),
-            motivation_letter = request.POST.get('motivation_letter'))
+                name = request.POST.get('name'),
+                surname = request.POST.get('surname'),
+                birth_date = request.POST.get('birth_date'),
+                phone = request.POST.get('phone'),
+                email = request.POST.get('email'),
+                motivation_letter = request.POST.get('motivation_letter'))
         
         if request.POST.get('message_text'):
             sender_email = request.POST.get('sender_email')
             sender_name = request.POST.get('sender_name')
             subject = 'Challengers Club-a xoş gəldin !'
-            message = 'Hörmətli ' + str(sender_name) + '! \nMüraciətiniz üçün təşəkkür edirik. Tezliklə sualınız cavablandırılacaq.'
+            message = 'Hörmətli ' + str(sender_name) + '! \nSualınız tezliklə cavablandırılacaq. Müraciətiniz üçün təşəkkür edirik. '
             from_email = settings.SERVER_EMAIL
             recipient_list = [sender_email]
             send_mail(subject, message, from_email, recipient_list)
