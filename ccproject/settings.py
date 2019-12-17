04202126f6cdccbe95885d5fd0f9c9674c26568d
Kamran Mmmdv <amorfrap@gmail.com>
2019-12-17 23:16:10 +0400

    Deleted some rows from admin.py

--- ccproject/settings.py
+++ ccproject/settings.py
@@ -103,10 +103,6 @@ SERVER_EMAIL = 'hi@challengers.az'
 EMAIL_HOST_USER = 'hi@challengers.az'
 EMAIL_HOST_PASSWORD = 'Qartdere1'
 
-# SERVER_EMAIL = 'kamransrecovery@gmail.com'
-# EMAIL_HOST_USER = 'kamransrecovery@gmail.com'
-# EMAIL_HOST_PASSWORD = 'kamran284'
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
